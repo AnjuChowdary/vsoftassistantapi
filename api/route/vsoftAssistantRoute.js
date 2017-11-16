5cbbebe338ee424ee680ec84a5521c03ba1f0f42
Anju <anjuchowdary93@gmail.com>
2017-11-16 12:40:15 +0530

    get call removed

--- api/route/vsoftAssistantRoute.js
+++ api/route/vsoftAssistantRoute.js
@@ -4,7 +4,7 @@ module.exports = function(app){
 
   //empList Routes
   app.route('/branch')
-    .get(emp.get_branch)
+    // .get(emp.get_branch)
     .post(emp.get_branchname)
 
   // app.route('/employee/:empName')
