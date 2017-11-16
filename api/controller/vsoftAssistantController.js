cf84da6d18d1343d9ab0b20e4939d254f0266e01
Anju <anjuchowdary93@gmail.com>
2017-11-16 16:17:19 +0530

    response changed

--- api/controller/vsoftAssistantController.js
+++ api/controller/vsoftAssistantController.js
@@ -1,7 +1,22 @@
 'use strict';
 
 exports.get_branch = function(req, res){
-    res.send("Madhapur");
+
+    var responseJson = {};
+
+      // If speech or displayText is defined, use it to respond (if one isn't defined use the other's value)
+      responseJson.speech = responseToUser.speech || responseToUser.displayText;
+      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
+
+      // Optional: add rich messages for integrations (https://dialogflow.com/docs/rich-messages)
+      responseJson.data = responseToUser.richResponses;
+
+      // Optional: add contexts (https://dialogflow.com/docs/contexts)
+      responseJson.contextOut = responseToUser.outputContexts;
+
+      // response.json(responseJson);
+
+      res.send(responseJson);
 };
 
 // exports.get_branchname = function(req, res){
